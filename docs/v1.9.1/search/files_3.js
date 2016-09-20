bed9253d920e5c7884d2c741c1bea84ff10f7021
Xiaocheng Tang <xiaocheng.t@gmail.com>
2016-09-20 11:29:43 -0700

    Update doc for 1.9.1 release

---
+++ docs/v1.9.1/search/files_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['data_5fpreparation_2esql_5fin',['data_preparation.sql_in',['../data__preparation_8sql__in.html',1,'']]],
+  ['decision_5ftree_2esql_5fin',['decision_tree.sql_in',['../decision__tree_8sql__in.html',1,'']]],
+  ['dense_5flinear_5fsystems_2esql_5fin',['dense_linear_systems.sql_in',['../dense__linear__systems_8sql__in.html',1,'']]],
+  ['distribution_2esql_5fin',['distribution.sql_in',['../distribution_8sql__in.html',1,'']]]
+];
---
+++ docs/v1.9.1/search/groups_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['arima',['ARIMA',['../group__grp__arima.html',1,'']]],
+  ['array_20operations',['Array Operations',['../group__grp__array.html',1,'']]],
+  ['arrays_20and_20matrices',['Arrays and Matrices',['../group__grp__arraysmatrix.html',1,'']]],
+  ['apriori_20algorithm',['Apriori Algorithm',['../group__grp__assoc__rules.html',1,'']]],
+  ['association_20rules',['Association Rules',['../group__grp__association__rules.html',1,'']]]
+];
