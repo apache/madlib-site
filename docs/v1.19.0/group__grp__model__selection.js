0deaaf8466842a41419873ef79fe2bfef389fcab
Orhan Kislal <okislal@apache.org>
2022-03-08 17:31:49 -0500

    Add 1.19.0 docs

---
+++ docs/v1.19.0/group__grp__model__selection.js
@@ -0,0 +1,6 @@
+var group__grp__model__selection =
+[
+    [ "Define Model Configurations", "group__grp__keras__setup__model__selection.html", null ],
+    [ "Train Model Configurations", "group__grp__keras__run__model__selection.html", null ],
+    [ "AutoML", "group__grp__automl.html", null ]
+];
---
+++ docs/v1.19.0/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghiklmnoprstuvwz",
+  1: "abcdeghiklmoprstuvw",
+  2: "_abcdefghiklmnoprstuvwz",
+  3: "eirv",
+  4: "abcdefghiklmnoprstuvw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables",
+  4: "groups"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables",
+  4: "Modules"
+};
+
