728b97226b9585e920ab7bfe8b9d39bd5ea00fa9
Ed Espino <espino@apache.org>
2023-04-11 03:18:42 -0700

    Remove unused Google Analytics support.

--- master.js
+++ master.js
@@ -98,17 +98,3 @@ jQuery(document).ready(function () {
         jQuery('#menu-item-68').removeClass('current-menu-item');
   });
 });
-
-
-//Google analytics1
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-45382226-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
